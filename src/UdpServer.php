f7b5f3b1f8228fde008def1a27ccfd0c07b482a7
Jian Liu <onanying@126.com>
2019-05-07 19:30:47 +0800

    更新

--- src/UdpServer.php
+++ src/UdpServer.php
@@ -114,7 +114,9 @@ class UdpServer extends AbstractObject
         // 绑定事件
         $this->_server->on(SwooleEvent::START, [$this, 'onStart']);
         $this->_server->on(SwooleEvent::MANAGER_START, [$this, 'onManagerStart']);
+        $this->_server->on(SwooleEvent::MANAGER_STOP, [$this, 'onManagerStop']);
         $this->_server->on(SwooleEvent::WORKER_START, [$this, 'onWorkerStart']);
+        $this->_server->on(SwooleEvent::WORKER_STOP, [$this, 'onWorkerStop']);
         $this->_server->on(SwooleEvent::PACKET, [$this, 'onPacket']);
         // 欢迎信息
         $this->welcome();
