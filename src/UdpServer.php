f509bcd279c71a5f9e134da8755a3adfbf41fd5f
Jian Liu <onanying@126.com>
2019-05-15 19:59:20 +0800

    修改统计类回调为success/error

--- src/UdpServer.php
+++ src/UdpServer.php
@@ -50,43 +50,45 @@ class UdpServer extends AbstractObject
      */
     protected $_defaultSetting = [
         // 开启协程
-        'enable_coroutine'   => true,
+        'enable_coroutine'    => true,
         // 主进程事件处理线程数
-        'reactor_num'        => 8,
+        'reactor_num'         => 8,
         // 工作进程数
-        'worker_num'         => 8,
+        'worker_num'          => 8,
         // 任务进程数
-        'task_worker_num'    => 0,
+        'task_worker_num'     => 0,
         // PID 文件
-        'pid_file'           => '/var/run/mix-udpd.pid',
+        'pid_file'            => '/var/run/mix-udpd.pid',
         // 日志文件路径
-        'log_file'           => '/tmp/mix-udpd.log',
+        'log_file'            => '/tmp/mix-udpd.log',
         // 异步安全重启
-        'reload_async'       => true,
+        'reload_async'        => true,
         // 退出等待时间
-        'max_wait_time'      => 60,
+        'max_wait_time'       => 60,
         // 开启后，PDO 协程多次 prepare 才不会有 40ms 延迟
-        'open_tcp_nodelay'   => true,
+        'open_tcp_nodelay'    => true,
         // 进程的最大任务数
-        'max_request'        => 0,
+        'max_request'         => 0,
         // 主进程启动事件回调
-        'hook_start'         => null,
+        'hook_start'          => null,
         // 主进程停止事件回调
-        'hook_shutdown'      => null,
+        'hook_shutdown'       => null,
         // 管理进程启动事件回调
-        'hook_manager_start' => null,
+        'hook_manager_start'  => null,
         // 工作进程错误事件
-        'hook_worker_error'  => null,
+        'hook_worker_error'   => null,
         // 管理进程停止事件回调
-        'hook_manager_stop'  => null,
+        'hook_manager_stop'   => null,
         // 工作进程启动事件回调
-        'hook_worker_start'  => null,
+        'hook_worker_start'   => null,
         // 工作进程停止事件回调
-        'hook_worker_stop'   => null,
+        'hook_worker_stop'    => null,
         // 工作进程退出事件回调
-        'hook_worker_exit'   => null,
-        // 监听数据事件回调
-        'hook_packet'        => null,
+        'hook_worker_exit'    => null,
+        // 监听数据成功回调
+        'hook_packet_success' => null,
+        // 监听数据错误回调
+        'hook_packet_error'   => null,
     ];
 
     /**
@@ -305,13 +307,13 @@ class UdpServer extends AbstractObject
                 $clientInfo
             );
             // 执行回调
-            $this->_setting['hook_packet'] and call_user_func($this->_setting['hook_packet'], true, $server, $clientInfo);
+            $this->_setting['hook_packet_success'] and call_user_func($this->_setting['hook_packet_success'], $server, $clientInfo);
 
         } catch (\Throwable $e) {
             // 错误处理
             \Mix::$app->error->handleException($e);
             // 执行回调
-            $this->_setting['hook_packet'] and call_user_func($this->_setting['hook_packet'], false, $server, $clientInfo);
+            $this->_setting['hook_packet_error'] and call_user_func($this->_setting['hook_packet_error'], $server, $clientInfo);
 
         } finally {
             // 清扫组件容器(仅同步模式, 协程会在xgo内清扫)
